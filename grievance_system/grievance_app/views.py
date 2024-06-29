e19844d72eaa85a593de71cefc9464341d314c7f
Tirth_ <128807701+Tirth9000@users.noreply.github.com>
2024-06-29 08:08:07 +0000

    modified

--- grievance_system/grievance_app/views.py
+++ grievance_system/grievance_app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from grievance_app.models import * 
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def login(request):
+    if request.method == 'POST':
+        user_email = request.POST.get('user_email')
+        password = request.POST.get('password')
+        print(user_email)
+        print(password)
+
+        user = authenticate(email=user_email, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('/emp_home')
+    return render(request, 'login.html')
+
+def emp_home(request):
+
+    return render(request, 'emp_home.html')
--- grievance_system/grievance_system/urls.py
+++ grievance_system/grievance_system/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from grievance_app.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', login, name ='login'),
+    path('emp_home', emp_home, name='emp_home'),
 ]
