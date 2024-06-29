bce27b06a8abc00f3bed40f3fa7ea2e1daad3b25
Tirth_ <128807701+Tirth9000@users.noreply.github.com>
2024-06-29 05:06:05 +0000

    Created Databases and basic setup for grievance

--- grievance_system/grievance_app/admin.py
+++ grievance_system/grievance_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from grievance_app.models import *
 
 # Register your models here.
+admin.site.register(grievance_db)
+admin.site.register(Employee_db)
+admin.site.register(HR_db)
+admin.site.register(Administrator_db)
--- grievance_system/grievance_app/models.py
+++ grievance_system/grievance_app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+class Employee_db(AbstractBaseUser):
+    username = models.CharField(primary_key = True, max_length = 50)
+    firstname = models.CharField(max_length = 100)
+    emp_dept = models.CharField(max_length = 50)
+    phone_no = models.CharField(max_length = 10)
+    email = models.EmailField(max_length = 256)
+    password = models.CharField(max_length = 100)
+
+
+class HR_db(AbstractBaseUser):
+    username = models.CharField(primary_key = True, max_length = 50)
+    firstname = models.CharField(max_length = 100)
+    HR_dept = models.CharField(max_length = 50)
+    phone_no = models.CharField(max_length = 10)
+    email = models.EmailField(max_length = 256)
+    password = models.CharField(max_length = 100)
+
+
+class Administrator_db(AbstractBaseUser):
+    username = models.CharField(primary_key = True, max_length = 50)
+    firstname = models.CharField(max_length = 100)
+    Admin_dept = models.CharField(max_length = 50)
+    phone_no = models.CharField(max_length = 10)
+    email = models.EmailField(max_length = 256)
+    password = models.CharField(max_length = 100)
+
+
+class grievance_db(models.Model):
+    G_id = models.CharField(primary_key = True, max_length = 50)
+    user_id = models.CharField(max_length = 50)
+    G_title = models.CharField(max_length = 100)
+    G_desc = models.TextField()
+    user_dept = models.CharField(max_length = 100)
+    severity = models.IntegerField(max_length = 3)
+    files = models.FileField(upload_to = "G_media/")
+    register_time = models.TimeField(auto_now_add = True)
+    completion_time = models.TimeField(auto_now_add = True)
+    statue = models.CharField(max_length = 50)
